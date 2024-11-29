4923d8143755cea85588604fa5cb0b30314e8ab1
Stuart Langley <stuart.langley@sequins.bio>
2024-11-29 05:06:06 +0000

    ci: install codecov-cli in the devcontainer
    Install codecov-cli in the devcontainer so we can
    manually upload coverage reports to codecov.io
    when unit testing inside the devcontainer.
    
    Had to change the base image to bullseye to work
    around an issue installed python modules in
    bookworm.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,13 @@
 
 #!/bin/bash
 
-set -e
+set -e -x
+
+# Ensure we have the latest version of pip
+pip install --upgrade pip
+
+# Required to do manual pushes to codecov from inside our container.
+pip install --user -r ./.devcontainer/requirements.txt
 
 # Install container dependencies.
 cargo install cargo-tarpaulin cargo-llvm-cov
