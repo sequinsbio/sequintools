59a3a07ce418fdceddb4a9f0ac1e640d6e4dd34f
Stuart Langley <stuart.langley@sequins.bio>
2024-12-23 23:53:25 +0000

    ci: remove codecov from devcontainer
    In guthub we no longer use the devcontainer for
    testing, so we can remove the dependancy on
    the codecov cli tool.
    
    With this removed, we can now use a rust
    devcontainer rather than a python one. Change
    the Dockerfile to use the rust devcontainer and
    remove rust from the features section of the
    devcontainer.json file.
    
    TEST=devcontainer stats && cargo test
    
    Fixes: #118

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,11 +3,5 @@
 
 set -e -x
 
-# Ensure we have the latest version of pip
-pip install --upgrade pip
-
-# Required to do manual pushes to codecov from inside our container.
-pip install --user -r ./.devcontainer/requirements.txt
-
 # Install container dependencies.
 cargo install cargo-tarpaulin@0.31.3
