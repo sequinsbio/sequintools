e6a6b8cdfa70439d52a38b3903f080aa50c1067f
Jonathan Ellis <jonathan.ellis@sequins.bio>
2025-09-10 14:27:09 +1000

    test: added calibrate CRAM io e2e tests (#248)
    Added e2e tests for using CRAM as input and/or output. These check the
    MD5 against pre-computed values for files in `testdata` and are design
    to stop any changes that cause a difference in output unless it is
    explicitly acknowledge.

--- tests/cli_integration.rs
+++ tests/cli_integration.rs
@@ -1,3 +1,4 @@
+use rust_htslib::bam::{self, Read};
 use std::fs;
 use std::fs::File;
 use std::process::{Command, Stdio};
@@ -65,7 +66,7 @@ fn test_calibrate_fixed_coverage() {
     let file_bytes = fs::read(&output_path).expect("Should have been able to read the file");
     let digest = md5::compute(&file_bytes);
     let computed_md5 = format!("{digest:x}");
-    let expected_md5 = "a7c2c35e7fe0f9459c96d93cc893aa70";
+    let expected_md5 = "f7441d6fac943fcc1649af48d34ef77c";
     assert_eq!(computed_md5, expected_md5, "MD5 checksum does not match");
 }
 
@@ -99,7 +100,7 @@ fn test_calibrate_sample_mean_coverage() {
     let file_bytes = fs::read(&output_path).expect("Should have been able to read the file");
     let digest = md5::compute(&file_bytes);
     let computed_md5 = format!("{digest:x}");
-    let expected_md5 = "524db272c7eb68049a3c4b72ab307f35";
+    let expected_md5 = "c074c15845ef58ef9ade8498e88ef264";
     assert_eq!(computed_md5, expected_md5, "MD5 checksum does not match");
 }
 
@@ -134,6 +135,114 @@ fn test_calibrate_sample_coverage_profile() {
     let file_bytes = fs::read(&output_path).expect("Should have been able to read the file");
     let digest = md5::compute(&file_bytes);
     let computed_md5 = format!("{digest:x}");
-    let expected_md5 = "8071d48d8130b2b6a7d459b51b662356";
+    let expected_md5 = "2bab127d2a1f63b7a9cf3a68f587228f";
     assert_eq!(computed_md5, expected_md5, "MD5 checksum does not match");
+}
+
+#[test]
+fn test_calibrate_cram_input() {
+    let temp_dir = TempDir::new().unwrap();
+    let output_path = temp_dir.path().join("calibrated.bam");
+    let output = Command::new("cargo")
+        .args([
+            "run",
+            "--bin",
+            "sequintools",
+            "calibrate",
+            "--bed",
+            "testdata/resources/sequin_regions.chrQ_mirror.bed",
+            "-o",
+            output_path.to_str().unwrap(),
+            "-T",
+            "testdata/genome_with_sequins.fasta",
+            "testdata/uncalibrated.cram",
+        ])
+        .output()
+        .expect("Failed to execute command");
+    assert!(
+        output.status.success(),
+        "Command failed with stderr: {}",
+        String::from_utf8_lossy(&output.stderr)
+    );
+    let file_bytes = fs::read(&output_path).expect("Should have been able to read the file");
+    let digest = md5::compute(&file_bytes);
+    let computed_md5 = format!("{digest:x}");
+    let expected_md5 = "64bf98716f2bc26ab82e3b4a0be24b55";
+    assert_eq!(computed_md5, expected_md5, "MD5 checksum does not match");
+}
+
+// We can't check MD5 because the CRAM output may vary in non-deterministic ways
+// on different machines. This is due to the choice of compression level and
+// other factors. However, all output files (BAM and CRAM) should have the same
+// content.
+
+// These aren't very robust tests, but it's difficult to check every aspect of
+// every read to ensure identity.
+
+#[test]
+fn test_calibrate_cram_output() {
+    let temp_dir = TempDir::new().unwrap();
+    let output_path = temp_dir.path().join("calibrated.cram");
+    let output = Command::new("cargo")
+        .args([
+            "run",
+            "--bin",
+            "sequintools",
+            "--",
+            "calibrate",
+            "--bed",
+            "testdata/resources/sequin_regions.chrQ_mirror.bed",
+            "-T",
+            "testdata/genome_with_sequins.fasta",
+            "--cram",
+            "-o",
+            output_path.to_str().unwrap(),
+            "testdata/uncalibrated.bam",
+        ])
+        .output()
+        .expect("Failed to execute command");
+    assert!(
+        output.status.success(),
+        "Command failed with stderr: {}",
+        String::from_utf8_lossy(&output.stderr)
+    );
+
+    let mut reader =
+        bam::Reader::from_path(output_path).expect("Should be able to open output CRAM");
+    let record_count = reader.records().count();
+    assert_eq!(record_count, 4718);
+}
+
+#[test]
+fn test_calibrate_cram_input_output() {
+    let temp_dir = TempDir::new().unwrap();
+    let output_path = temp_dir.path().join("calibrated.cram");
+    let output = Command::new("cargo")
+        .args([
+            "run",
+            "--bin",
+            "sequintools",
+            "--",
+            "calibrate",
+            "--bed",
+            "testdata/resources/sequin_regions.chrQ_mirror.bed",
+            "-T",
+            "testdata/genome_with_sequins.fasta",
+            "--cram",
+            "-o",
+            output_path.to_str().unwrap(),
+            "testdata/uncalibrated.cram",
+        ])
+        .output()
+        .expect("Failed to execute command");
+    assert!(
+        output.status.success(),
+        "Command failed with stderr: {}",
+        String::from_utf8_lossy(&output.stderr)
+    );
+
+    let mut reader =
+        bam::Reader::from_path(output_path).expect("Should be able to open output CRAM");
+    let record_count = reader.records().count();
+    assert_eq!(record_count, 4718);
 }
