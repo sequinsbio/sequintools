4a6400335e1c6a97be67c71efcf94761d6ee4071
Jonathan Ellis <jonathan.ellis@sequins.bio>
2025-09-10 14:27:31 +1000

    feat: update cli version message (#251)
    Include `git describe` in version message produced with `sequintools
    --version`. This will now include how many commits from the last tag and
    the commit hash used to build the binary. This can be vital in
    reproducing bugs or understanding inconsistencies in output. If built
    from latest commit, it may be many commits since the last tag, so just
    using this is not helpful.

---
+++ build.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::process::Command;
+
+fn main() {
+    let version = env::var("CARGO_PKG_VERSION").unwrap();
+
+    let git_output = Command::new("git")
+        .args(["describe", "--tags", "--dirty", "--always"])
+        .output()
+        .ok()
+        .and_then(|o| String::from_utf8(o.stdout).ok())
+        .unwrap_or_else(|| version.clone());
+
+    println!("cargo:rustc-env=GIT_VERSION={}", git_output.trim());
+}
