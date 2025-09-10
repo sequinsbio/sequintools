d463e1d914d63e512247545e0a3b26aa1e495096
Jonathan Ellis <jonathan.ellis@sequins.bio>
2025-09-10 17:11:17 +1000

    ci: remove .git from .dockerignore
    A previous commit updated the way the version is detected in a way that
    requires the git directory (`.git`). This was not being sent to the
    docker daemon when building containers causing a failure. This commit
    removes the git directory from the `.dockerignore` file and makes the
    build function more robust when `.git` is missing.

--- build.rs
+++ build.rs
@@ -9,6 +9,8 @@ fn main() {
         .output()
         .ok()
         .and_then(|o| String::from_utf8(o.stdout).ok())
+        .map(|s| s.trim().to_string())
+        .filter(|s| !s.is_empty())
         .unwrap_or_else(|| version.clone());
 
     println!("cargo:rustc-env=GIT_VERSION={}", git_output.trim());
