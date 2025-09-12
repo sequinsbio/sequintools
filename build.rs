127987e0a855335fd2579192dddf4beb80b1839c
Jonathan Ellis <jonathan.ellis@sequins.bio>
2025-09-12 11:26:37 +1000

    fix: update docker to use arg instead of copying .git
    Apparently adding .git is non-idiomatic and frowned upon. Changing the
    dockerfile and action to use an arg instead.

--- build.rs
+++ build.rs
@@ -2,22 +2,26 @@ use std::env;
 use std::process::Command;
 
 fn main() {
-    let version = env::var("CARGO_PKG_VERSION").unwrap();
-
-    let git_output = Command::new("git")
-        .args(["describe", "--tags", "--dirty", "--always"])
-        .output()
-        .ok()
-        .and_then(|o| String::from_utf8(o.stdout).ok())
-        .and_then(|s| {
-            let trimmed = s.trim();
-            if trimmed.is_empty() {
-                None
-            } else {
-                Some(trimmed.to_string())
-            }
-        })
-        .unwrap_or_else(|| version.clone());
-
-    println!("cargo:rustc-env=GIT_VERSION={}", git_output);
+    let git_version = match env::var("SEQUINTOOLS_GIT_VERSION") {
+        Ok(version) => version,
+        Err(_) => {
+            let version = env::var("CARGO_PKG_VERSION").unwrap();
+            let git_output = Command::new("git")
+                .args(["describe", "--tags", "--dirty", "--always"])
+                .output()
+                .ok()
+                .and_then(|o| String::from_utf8(o.stdout).ok())
+                .and_then(|s| {
+                    let trimmed = s.trim();
+                    if trimmed.is_empty() {
+                        None
+                    } else {
+                        Some(trimmed.to_string())
+                    }
+                })
+                .unwrap_or_else(|| version.clone());
+            git_output
+        }
+    };
+    println!("cargo:rustc-env=GIT_VERSION={}", git_version);
 }
